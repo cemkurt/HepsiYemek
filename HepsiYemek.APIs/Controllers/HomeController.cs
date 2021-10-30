22599f479aaa167762becaa2de764936b3b83f30
cemkurt <cem.net@hotmail.com>
2021-10-30 20:20:34 +0300

    son2

--- HepsiYemek.APIs/Controllers/HomeController.cs
+++ HepsiYemek.APIs/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
+using HepsiYemek.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace HepsiYemek.APIs.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class HomeController  : ControllerBase
     {
+        
+        [HttpGet, Route("list")]
+        [SwaggerOperation(Summary = "Product listesini verir, name parametresinde arama yapar", Description = "")]
+        [ProducesResponseType(typeof(ProductResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponseDto), StatusCodes.Status400BadRequest)]
         public IActionResult Index()
         {
 
